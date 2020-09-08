564df43471d34fafc21781c68aede00a85c8c6c5
victoriiakaplun <victoriaa.kaplun@gmail.com>
2020-09-08 14:23:08 +0600

    [+] task 1 was added

--- Hello World/app/src/main/java/ru/omsu/imit/helloworld/HelloWorld.java
+++ Hello World/app/src/main/java/ru/omsu/imit/helloworld/HelloWorld.java
@@ -2,16 +2,29 @@ package ru.omsu.imit.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class HelloWorld extends Activity {
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        TextView textView = new TextView(this);
-        textView.setText(getString(R.string.hello));
-        setContentView(textView);
+        mHelloTextView = findViewById(R.id.textView);
+        mNameEditText = findViewById(R.id.editTextTextPersonName);
+    }
+
+    public void onClick(View view) {
+        if(mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Kitty!");
+        } else {
+            mHelloTextView.setText("Привет, " + mNameEditText.getText());
+        }
     }
 }
